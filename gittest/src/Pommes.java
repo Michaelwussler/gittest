ba53f956647f265780140e0f532133130a73435f
Michaelwussler <michaelwussler@freenet.de>
2012-07-12 13:39:57 +0200

    Michael Branch?

--- gittest/src/Pommes.java
+++ gittest/src/Pommes.java
@@ -5,8 +5,10 @@ public class Pommes {
 
 	public static boolean mitKetchup;
 	public static boolean mitMayo=true;
+	public int jetzt=3;
 	
 	public static boolean rotWeiss(){
+	
 		if (Pommes.mitKetchup && Pommes.mitMayo){
 			return true;
 		}

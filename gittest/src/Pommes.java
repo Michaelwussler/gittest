cf7fb0e554d6b348b86da16f51142f3e595d36a5
Peter Raiser <praiser@gmx.de>
2012-07-12 13:08:32 +0200

    test3

--- gittest/src/Pommes.java
+++ gittest/src/Pommes.java
@@ -1,4 +1,16 @@
 
 public class Pommes {
 
+	public static boolean mitKetchup;
+	public static boolean mitMayo;
+	
+	public static boolean rotWeiss(){
+		if (Pommes.mitKetchup && Pommes.mitMayo){
+			return true;
+		}
+		else
+			return false;
+	}
+	
+	
 }
